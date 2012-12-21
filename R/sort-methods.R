954ca3ea308e65d0d062a81b17160b1d0a5d724b
Martin Morgan <mtmorgan@fhcrc.org>
2012-12-21 02:39:29 +0000

    code clean-up
    - 'sort' method conforms to signature
    - explicitly import(methods), re-use BiocGenerics methods as appropriate
    - remove empty sections from man pages
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/KCsmart@72096 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/sort-methods.R
+++ R/sort-methods.R
@@ -1,11 +1,11 @@
-setMethod("sort", signature("KcghData", "missing"), function(x){
+setMethod("sort", signature("KcghData"), function(x, decreasing=FALSE, ...){
     dataOrdering <- order(x@probeAnnotation@chromosome, x@probeAnnotation@maploc)
     x@probeAnnotation <- x@probeAnnotation[dataOrdering]
     x@data <- x@data[dataOrdering,]
     x
 })
 
-setMethod("sort", signature("KcghDataSum", "missing"), function(x){
+setMethod("sort", signature("KcghDataSum"), function(x, decreasing=FALSE, ...){
     dataOrdering <- order(x@probeAnnotation@chromosome, x@probeAnnotation@maploc)
     x@probeAnnotation <- x@probeAnnotation[dataOrdering]
     x@pos <- x@pos[dataOrdering]
